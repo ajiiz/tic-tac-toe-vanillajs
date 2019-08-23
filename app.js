9f142d068b22fc7db42218fe9d338ec0cd3186ca
ajiiz <opticpiter@gmail.com>
2019-08-23 20:55:36 +0200

    Clicking on board element now displays X or O depending on which player turn is it

--- app.js
+++ app.js
@@ -9,28 +9,38 @@ const game = () => {
 
     const maxValue = 9;
     let boardLength = 0;
+    const player0 = "0[O]";
+    const player1 = "1[X]";
 
     const places = document.querySelectorAll(".main-game__place");
+    const playerName = document.getElementById("playerName");
 
         places.forEach((insert,index) => {
             insert.addEventListener('click',() => {
+                let position = document.getElementById(`${index + 1}`);
+                console.log(position);
                     if(player) {
                         if(board[index] != 'X' && board[index] != 'O') {
                             board[index] = 'X';
+                            position.textContent = "X";
                             console.log(board[index]);
                             boardLength++;
+                            playerName.textContent = "Now playing: Player " + player0;
                             player = 0;
                         }
                     } else if(!player) {
                         if(board[index] != 'X' && board[index] != 'O') {
+                            position.textContent = "O";
                             board[index] = 'O';
                             console.log(board[index]);
                             boardLength ++;
+                            playerName.textContent = "Now playing: Player " + player1;
                             player = 1;
                         }
                     }
                     if(boardLength == maxValue) {
-                        gameEnded(board);
+                        gameEnded(board, playerName);
+                        boardLength++;
                     }
             });
         });
@@ -40,9 +50,10 @@ const gameCheck = () => {
 
 }
 
-const gameEnded = (board) => {
+const gameEnded = (board, playerName) => {
     console.table(board);
     console.log('game Ended');
+    playerName.textContent = "Game has ended";
 }
 
 game();
