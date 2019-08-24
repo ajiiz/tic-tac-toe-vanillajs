1e41c4b13bcee2f22d89fa115952494c8e0b6034
ajiiz <opticpiter@gmail.com>
2019-08-24 18:52:45 +0200

    Implemented function that cheks if someone has already won and function that displays a winner if there is on

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
-const game = () => {
+const gameStart = () => {
 
     const board =   ['', '', '',
                      '', '', '',
                      '', '', ''];
     let player = 1;
     // 1 = X
-    // 0 = O
+    // 2 = O
 
-    const maxValue = 9;
     let boardLength = 0;
-    const player0 = "0[O]";
-    const player1 = "1[X]";
+
+    const player1 = "1 (X)"; //Player 1 = X
+    const player2 = "2 (O)"; //Player 2 = O
+
 
     const places = document.querySelectorAll(".main-game__place");
     const playerName = document.getElementById("playerName");
+
+    playerName.textContent = "Click on the board to start!";
 
         places.forEach((insert,index) => {
             insert.addEventListener('click',() => {
                 let position = document.getElementById(`${index + 1}`);
                 console.log(position);
-                    if(player) {
+                    if(player == 1) {
                         if(board[index] != 'X' && board[index] != 'O') {
                             board[index] = 'X';
                             position.textContent = "X";
                             console.log(board[index]);
                             boardLength++;
-                            playerName.textContent = "Now playing: Player " + player0;
-                            player = 0;
+                            playerName.textContent = "Now playing: Player " + player2;
+                            player = 2;
                         }
-                    } else if(!player) {
+                    } else if(player == 2) {
                         if(board[index] != 'X' && board[index] != 'O') {
                             position.textContent = "O";
                             board[index] = 'O';
@@ -38,22 +41,52 @@ const game = () => {
                             player = 1;
                         }
                     }
-                    if(boardLength == maxValue) {
-                        gameEnded(board, playerName);
-                        boardLength++;
+                    if(boardLength >= 3) {
+                        gameCheck(player, board, playerName, boardLength);
                     }
             });
         });
     };
 
-const gameCheck = () => {
-
+const gameCheck = (player,board,playerName, boardLength) => {
+    // Checking rows
+    if(board[0] == board[1] && board[0] == board[2] && board[1] == board[2]) {
+        gameEnded(board, playerName, player);
+    } if(board[3] == board[4] && board[3] == board[5] && board[4] == board[5]) {
+        gameEnded(board, playerName, player);
+    } if(board[6] == board[7] && board[6] == board[8] && board[7] == board[8]) {
+        gameEnded(board, playerName, player);
+    }
+    // Checking columns
+    if(board[0] == board[3] && board[0] == board[6] && board[3] == board[6]) {
+        gameEnded(board, playerName, player);
+    } if(board[1] == board[4] && board[1] == board[7] && board[4] == board[7]) {
+        gameEnded(board, playerName, player);
+    } if(board[2] == board[5] && board[2] == board[8] && board[5] == board[8]) {
+        gameEnded(board, playerName, player);
+    }
+    //Checking Diagonals
+    if(board[0] == board[4] && board[0] == board[8] && board[4] == board[8]) {
+        gameEnded(board, playerName, player);
+    } if(board[2] == board[4] && board[2] == board[6] && board[4] == board[6]) {
+        gameEnded(board, playerName, player);
+    }
+    if(board.length == boardLength) {
+        player = 0;
+        gameEnded(board, playerName, player);
+    }
 }
 
-const gameEnded = (board, playerName) => {
-    console.table(board);
-    console.log('game Ended');
-    playerName.textContent = "Game has ended";
+const gameEnded = (board, playerName, player) => {
+    if(player == 1) {
+        playerName.textContent = "Player 1 has won! (X)";
+    }
+    else if(player == 2) {
+        playerName.textContent = "Player 2 has won! (O)";
+    }
+    else if(player == 0){
+        playerName.textContent = "Game has ended as Draw";
+    }
 }
 
-game();
+gameStart();
